383ae1ff84e892844f7c2e522f47685805430f3f
ramyologist <88153202+ramyologist@users.noreply.github.com>
2024-03-13 23:21:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,25 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Angenommen, die ersten fünf Fragen (nach Datum, Session und Voter) sind für das kulturelle Kapital
+# und die nächsten fünf Fragen für das ökonomische Kapital.
+KULTURELLES_KAPITAL_START = 3  # Beginn bei der vierten Spalte
+KULTURELLES_KAPITAL_END = 8  # Fünf Fragen für kulturelles Kapital
+OEKONOMISCHES_KAPITAL_START = 8  # Beginn bei der neunten Spalte
+OEKONOMISCHES_KAPITAL_END = 13  # Fünf Fragen für ökonomisches Kapital
+
 def laden_und_berechnen_der_daten(uploaded_file):
-    df = pd.read_excel(uploaded_file, skiprows=2)  # Überspringe die ersten beiden Zeilen
-    df.fillna(0, inplace=True)  # Ersetze alle NaN mit 0
+    df = pd.read_excel(uploaded_file, skiprows=2)
+    df.fillna(0, inplace=True)
 
     # Konvertiere Spalten in numerische Werte, falls notwendig
-    for col in df.columns[3:]:
+    for col in df.columns[KULTURELLES_KAPITAL_START:OEKONOMISCHES_KAPITAL_END]:
         df[col] = pd.to_numeric(df[col], errors='coerce')
 
     # Berechnung der Werte für das Diagramm
-    df['Kulturelles Kapital'] = df.iloc[:, 3:8].mean(axis=1)
-    df['Ökonomisches Kapital'] = df.iloc[:, 8:].mean(axis=1)
-    df['X-Wert'] = df['Kulturelles Kapital'] - df['Ökonomisches Kapital']
+    df['Kulturelles Kapital'] = df.iloc[:, KULTURELLES_KAPITAL_START:KULTURELLES_KAPITAL_END].mean(axis=1)
+    df['Ökonomisches Kapital'] = df.iloc[:, OEKONOMISCHES_KAPITAL_START:OEKONOMISCHES_KAPITAL_END].mean(axis=1)
+    df['X-Wert'] = df['Ökonomisches Kapital'] - df['Kulturelles Kapital']
     df['Y-Wert'] = df['Kulturelles Kapital'] + df['Ökonomisches Kapital']
 
     return df
@@ -31,8 +38,8 @@ def diagramm_erstellen(df):
     ax.set_xlim(x_min - x_puffer, x_max + x_puffer)
     ax.set_ylim(y_min - y_puffer, y_max + y_puffer)
     ax.set_title('Kreuzdiagramm des sozialen Raums')
-    ax.set_xlabel('Kulturelles vs. Ökonomisches Kapital')
-    ax.set_ylabel('Gesamtkapitalvolumen')
+    ax.set_xlabel('Ökonomisches Kapital (mehr ist links)')
+    ax.set_ylabel('Kulturelles Kapital (mehr ist oben)')
     ax.grid(True)
     ax.legend()
     return fig
